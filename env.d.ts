5804331e5673496a749a67a3ab2361d700385e9f
MosheOgalbo <65408488+MosheOgalbo@users.noreply.github.com>
2024-10-20 00:48:13 +0300

    new project

---
+++ env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+declare module 'vue-global-loader/GlobalLoader.vue';
+declare module 'vue-global-loader/CircleSpinner.vue'
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import GlobalLoader from 'vue-global-loader/GlobalLoader.vue';
+import CircleSpinner from 'vue-global-loader/CircleSpinner.vue'
+import { Notivue, Notifications } from 'notivue';
+import Navbar from './components/Navbar.vue'
+// import { globalLoader } from 'vue-global-loader'
+
+</script>
+
+<template>
+    <GlobalLoader>
+        <CircleSpinner />
+    </GlobalLoader>
+    <Notivue v-slot="item">
+        <Notifications :item="item" />
+    </Notivue>
+    <Navbar />
+    <RouterView />
+</template>
